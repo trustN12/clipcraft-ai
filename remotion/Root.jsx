ad436f717aa80a393f2b5770e44e52cbe1a9f610
NABARUN <nbiswas100@outlook.com>
2025-05-21 01:22:41 +0530

    implementation in remotion

--- remotion/Root.jsx
+++ remotion/Root.jsx
@@ -13,9 +13,7 @@ export const RemotionRoot = () => {
         fps={30}
         width={1280}
         height={720}
-        defaultProps={{
-          videoData: videoData
-        }}
+        defaultProps={{ videoData }}
       />
     </>
   );
